b6fb9f9c8b7dbc1f928ede054023998241e813d0
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-07-22 23:20:56 +0300

    Update __main__.py

--- src/octoprint/__main__.py
+++ src/octoprint/__main__.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 import os
+
+# ❗ Перемещаем установку переменной вверх
+os.environ["OCTOPRINT_CONFIG_DIR"] = os.path.expanduser("~/.3dquanter")
 
 if __name__ == "__main__":
     import octoprint
-    os.environ["OCTOPRINT_CONFIG_DIR"] = os.path.expanduser("~/.3dquanter")
     octoprint.main()
