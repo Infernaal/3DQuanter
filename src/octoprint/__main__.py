510ece5deb43d19312461d96be3674acc9b87deb
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-07-22 23:12:37 +0300

    Update __main__.py

--- src/octoprint/__main__.py
+++ src/octoprint/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+import os
 
 if __name__ == "__main__":
     import octoprint
-
+    os.environ["OCTOPRINT_CONFIG_DIR"] = os.path.expanduser("~/.3dquanter")
     octoprint.main()
